d2efbd8e2669647fc5f2070bc407bd8356e9a195
TruthVoyager <Christianm.jones10999@gmail.com>
2024-03-07 17:49:31 -0500

    Add project files.

---
+++ CodeChallenge/Services/ReportingService.cs
@@ -0,0 +1,38 @@
+﻿using CodeChallenge.Models;
+using CodeChallenge.Services;
+
+namespace CodeChallenge.Reporting
+{
+    public class ReportingService
+    {
+        private readonly EmployeeService _employeeService;
+
+        public ReportingService(EmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        private int CalculateNumberOfReportsRecursive(Employee employee)
+        {
+            int numberOfReports = 0;
+
+            // Count the direct reports of the current employee
+            numberOfReports += employee.DirectReports?.Count ?? 0;
+
+            // Recursively count the reports of each direct report
+            if (employee.DirectReports != null)
+            {
+                foreach (Employee Report in employee.DirectReports)
+                {
+                    Employee directReport = _employeeService.GetById(Report.EmployeeId);
+                    if (directReport != null)
+                    {
+                        numberOfReports += CalculateNumberOfReportsRecursive(directReport);
+                    }
+                }
+            }
+
+            return numberOfReports;
+        }
+    }
+}
