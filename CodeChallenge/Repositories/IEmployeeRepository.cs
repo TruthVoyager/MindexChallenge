de6321331bd83fdfb8d8ea211c54f69d8c8cc121
TruthVoyager <Christianm.jones10999@gmail.com>
2024-03-07 19:36:49 -0500

    Removed unimplemented method from interface and added missing semicolon.

--- CodeChallenge/Repositories/IEmployeeRepository.cs
+++ CodeChallenge/Repositories/IEmployeeRepository.cs
@@ -7,11 +7,10 @@ namespace CodeChallenge.Repositories
     public interface IEmployeeRepository
     {
         Employee GetById(String id);
-        ReportingStructure GetReportingServiceById(string id);
         Employee Add(Employee employee);
         Employee Remove(Employee employee);
         Compensation AddCompensation(Compensation compensation);
-        Compensation GetCompensationByEmployeeId(string employeeId)
+        Compensation GetCompensationByEmployeeId(string employeeId);
         Task SaveAsync();
     }
 }
